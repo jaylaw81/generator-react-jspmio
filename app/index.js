b2743eb0a96d95d84681837a347f5f5eb8e683df
Jason Law <jlaw@sapient.com>
2015-05-26 18:58:32 -0400

    added new options to packages

--- app/index.js
+++ app/index.js
@@ -15,12 +15,29 @@ var ReactSDPGenerator = yeoman.generators.Base.extend({
 			'\n' + chalk.bold.underline('Welcome to the React SDP Component Generator')
 		);
 
-		var prompts = [{
+		var prompts = [
+		{
+			type: 'input',
+			name: 'projectName',
+			message: 'What is the name of your project?',
+			default: 'CGSDP'
+		},
+		{
 			type: 'input',
 			name: 'componentName',
 			message: 'What is the name of the component?',
 			default: 'NewComponent'
-		}];
+		},
+		{
+			type: "list",
+			name: "repoType",
+			message: "Which package manager would you like to use?",
+			choices: [
+			  "jspm", "npm", "github", "stash"
+			],
+			default: "JSPM"
+		  }
+		];
 
 		this.prompt(prompts, function (props) {
 			this.log('\n');
@@ -35,14 +52,14 @@ var ReactSDPGenerator = yeoman.generators.Base.extend({
 
 	writing: {
 		project: function () {
-			this.template('client/jspm_packages/stash/CGSDP/base/_package.json', 'jspm_packages/stash/CGSDP/'+ this.componentKey +'/package.json');
+			this.template('client/jspm_packages/stash/'+this.projectName+'/base/_package.json', 'jspm_packages/'+this.repoType+'/'+this.projectName+'/'+ this.componentKey +'/package.json');
 		},
 
 		clientfiles: function() {
-			this.template('client/jspm_packages/stash/CGSDP/base/css/_style.scss', 'jspm_packages/stash/CGSDP/'+ this.componentKey +'/css/_style.scss');
-			this.template('client/jspm_packages/stash/CGSDP/base/_.jspm.json', 'jspm_packages/stash/CGSDP/'+ this.componentKey +'/.jspm.json');
-			this.template('client/jspm_packages/stash/CGSDP/_index.js', 'jspm_packages/stash/CGSDP/'+ this.componentKey +'.js');
-			this.directory('client/jspm_packages/stash/CGSDP/base/lib', 'jspm_packages/stash/CGSDP/'+ this.componentKey +'/lib');
+			this.template('client/jspm_packages/stash/CGSDP/base/css/_style.scss', 'jspm_packages/'+this.repoType+'/'+this.projectName+'/'+ this.componentKey +'/css/_style.scss');
+			this.template('client/jspm_packages/stash/CGSDP/base/_.jspm.json', 'jspm_packages/'+this.repoType+'/'+this.projectName+'/'+ this.componentKey +'/.jspm.json');
+			this.template('client/jspm_packages/stash/CGSDP/_index.js', 'jspm_packages/'+this.repoType+'/'+this.projectName+'/'+ this.componentKey +'.js');
+			this.directory('client/jspm_packages/stash/CGSDP/base/lib', 'jspm_packages/'+this.repoType+'/'+this.projectName+'/'+ this.componentKey +'/lib');
 
 		}
 	},
